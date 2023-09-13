c99d1f84591a59c4eb203c5fde27fdf76c56ece0
Michael Yuan <michael@secondstate.io>
2023-09-13 06:17:53 +0000

    Code and data example
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# demo-RAG-embeddings
+# Create and save embeddings from posted text
+
+If you want to build locally, make sure you have installed Rust and added the `wasm32-wasi` target.
+
+```
+cargo build --target wasm32-wasi --release
+```
+
+Reset the database and add embeddings from a file. Each file could contain multiple text segments. Each segment corresponds to an 
+embedding. The text sgements are seperated by blank lines in the text file, with exception of blank lines in code listings.
+Each code listing always belong to a segment, and it is never broken up.
+
+```
+curl "https://code.flows.network/lambda/2tVDJrikvo?collection_name=my_kb&vector_size=1536&reset=1" -X POST --data-binary "@test1.txt"
+```
+
+Append the database and add embeddings from a file
+
+```
+curl "https://code.flows.network/lambda/2tVDJrikvo?collection_name=my_kb&vector_size=1536" -X POST --data-binary "@test2.txt"
+```
+
---
+++ src/lib.rs
@@ -0,0 +1,125 @@
+use webhook_flows::{create_endpoint, request_handler, send_response};
+use vector_store_flows::*;
+use openai_flows::{
+    embeddings::{EmbeddingsInput},
+    OpenAIFlows,
+};
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::str;
+
+static CHAR_SOFT_LIMIT : usize = 20000;
+static CHAR_SOFT_MINIMUM : usize = 100;
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn on_deploy() {
+    create_endpoint().await;
+}
+
+#[request_handler]
+async fn handler(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, body: Vec<u8>) {
+    let collection_name = qry.get("collection_name").unwrap().as_str().unwrap();
+    let vector_size = qry.get("vector_size").unwrap().as_str().unwrap();
+    let vector_size : u64 = vector_size.parse().unwrap();
+    let mut id : u64 = 0;
+
+    if qry.contains_key("reset") {
+        log::debug!("Reset the database");
+        // Delete collection, ignore any error
+        _ = delete_collection(collection_name).await;
+        // Create collection
+        let p = CollectionCreateParams { vector_size: vector_size };
+        if let Err(e) = create_collection(collection_name, &p).await {
+            log::error!("Cannot create collection named: {} with error: {}", collection_name, e);
+            send_success("Cannot create collection");
+            return;
+        }
+    } else {
+        log::debug!("Continue with existing database");
+        match collection_info(collection_name).await {
+            Ok(ci) => {
+                id = ci.points_count;
+            }
+            Err(e) => {
+                log::error!("Cannot get collection stat {}", e);
+                send_success("Cannot query database!");
+                return;
+            }
+        }
+    }
+    log::debug!("Starting ID is {}", id);
+
+    let mut openai = OpenAIFlows::new();
+    openai.set_retry_times(3);
+
+    let s = str::from_utf8(&body).unwrap();
+    let mut points = Vec::<Point>::new();
+    let mut current_section = String::new();
+    let mut is_code_block = false;
+    for line in s.lines() {
+        // Append the line to the current section if the current section is less than CHAR_SOFT_LIMIT
+        if current_section.len() < CHAR_SOFT_LIMIT {
+            current_section.push_str(line);
+            current_section.push('\n');
+        } else {
+            log::warn!("Section already has {} chars. Exceeded CHAR_SOFT_LIMIT. Skipped line: {}", current_section.len(), line);
+        }
+
+        if line.trim().starts_with("```") {
+            is_code_block = !is_code_block;
+        }
+
+        if line.trim().is_empty() && (!is_code_block) && (current_section.len() > CHAR_SOFT_MINIMUM) {
+            if !current_section.trim().is_empty() {
+                // create and save embedding
+                let input = EmbeddingsInput::String(current_section.clone());
+                match openai.create_embeddings(input).await {
+                    Ok(r) => {
+                        for v in r.iter() {
+                            let p = Point {
+                                id: PointId::Num(id),
+                                vector: v.iter().map(|n| *n as f32).collect(),
+                                payload: json!({"text": current_section}).as_object().map(|m| m.to_owned()),
+                            };
+                            points.push(p);
+                            log::debug!("Created vector {} with length {}", id, v.len());
+                            id += 1;
+                        }
+                    }
+                    Err(e) => {
+                        log::error!("OpenAI returned an error: {}", e);
+                    }
+                }
+            }
+            // Start a new section
+            current_section.clear();
+        }
+    }
+    let points_count = points.len();
+
+    if let Err(e) = upsert_points(collection_name, points).await {
+        log::error!("Cannot upsert into database! {}", e);
+        send_success("Cannot upsert into database!");
+        return;
+    }
+
+    match collection_info(collection_name).await {
+        Ok(ci) => {
+            log::debug!("There are {} vectors in collection `{}`", ci.points_count, collection_name);
+            send_success(&format!("Successfully inserted {} records. The collection now has {} records in total.", points_count, ci.points_count));
+        }
+        Err(e) => {
+            log::error!("Cannot get collection stat {}", e);
+            send_success("Cannot upsert into database!");
+        }
+    }
+}
+
+fn send_success(body: &str) {
+    send_response(
+        200,
+        vec![(String::from("content-type"), String::from("text/html"))],
+        body.as_bytes().to_vec(),
+    );
+}
